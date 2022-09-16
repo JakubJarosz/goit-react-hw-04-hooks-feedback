141039df210c7cc2e8921935dacd70fecc0699b4
JakubJarosz <jakubtm165@gmail.com>
2022-09-16 15:23:26 +0200

    Updates

---
+++ precache-manifest.c3f6a24b6cbf40397d93abe73387cf1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43397df36412dd2de626",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/main.2bf0eb67.chunk.css"
+  },
+  {
+    "revision": "43397df36412dd2de626",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/main.ccf2f5eb.chunk.js"
+  },
+  {
+    "revision": "f0173b967620059a8df1",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/runtime~main.52105f87.js"
+  },
+  {
+    "revision": "5b414d1c5ab3ec8cfc2d",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.1a8d7fa0.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "6dcea484aaf33b675311283fb95b01c6",
+    "url": "/goit-react-hw-04-hooks-feedback/index.html"
+  }
+];
